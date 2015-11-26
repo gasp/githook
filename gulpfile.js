431d09d1cce69081b87125eb31addbe780e0758e
gaspard <gaspard@gmail.com>
2015-11-26 17:11:25 +0100

    gulpfile: running tests

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/* jshint node: true */
+
+'use strict';
+
+var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
+
+// test whatever is specified in tests/ folder
+gulp.task('tests', function () {
+  return gulp.src('tests/*.js')
+    .pipe(jasmine());
+});
