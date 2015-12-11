c82287a80ece6d8ef129027cff13441ef4145050
gaspard <gaspard@gmail.com>
2015-12-11 20:39:21 +0100

    tests: functional delivery

---
+++ tests/delivery.js
@@ -0,0 +1,56 @@
+/* jshint node: true, jasmine: true */
+
+// creates its own server instance on 4003
+// and tests it with a fake delivery payload
+
+var http = require('http');
+var server = require('../server');
+var port = 4003;
+
+server.listen(port);
+
+var querystring = require('querystring');
+var postData = JSON.stringify({
+  'ref': 'master',
+  'repository': {
+    'name': 'front-demo'
+  },
+  'sender': {
+    'login': 'testnode'
+  }
+});
+
+var options = {
+  hostname: 'localhost',
+  port: port,
+  path: '/',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': postData.length,
+    'x-github-event': 'test'
+  }
+};
+
+var req = http.request(options, function(res) {
+  // console.log('STATUS: ' + res.statusCode);
+  // console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    // console.log('BODY: ' + chunk);
+  });
+  res.on('end', function() {
+    // console.log('No more data in response.');
+    server.close(function () {
+      console.log('server closed');
+    });
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+req.write(postData);
+req.end();
