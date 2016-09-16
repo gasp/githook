53ef79aa856b38130f7d95ee550b3570b38a8092
florian <florian@videodesk.com>
2016-09-16 10:15:44 +0200

    add some tests

--- tests/queueing.js
+++ tests/queueing.js
@@ -2,19 +2,35 @@
  * Created by florian on 9/15/16.
  */
 
-'use strict';
-
 var queueing = require('../lib/queueing.js');
 
-describe('queueing', function() {
+describe('a queueing tests suite', function () {
 
-  it('test queue', function() {
+  var element = ['script','folder'];
+  queueing.addToQueue(element);
 
-    queueing.addToQueue('element');
+  it('has an element in queue', function () {
+    expect(queueing.getLength()).toEqual(1);
+  });
 
-    expect(queueing.getLength()).to.equal(1);
-    expect(queueing.findInQueue('element')).to.equal(true);
+  it('is "element"', function () {
+    expect(queueing.findInQueue(element)).toEqual(true);
+  });
 
+  it('is does not contains "element2"', function () {
+    var element2 = ['script2', 'folder2'];
+    expect(queueing.findInQueue(element2)).toEqual(false);
+  });
+
+  it('has an second element', function () {
+    var element3 = ['script3', 'folder3'];
+    queueing.addToQueue(element3);
+    expect(queueing.getLength()).toEqual(2);
+  });
+
+  it('has an element remove', function () {
+    queueing.RemoveFirstToQueue();
+    expect(queueing.getLength()).toEqual(1);
   });
 
 });
