38453dfd81d8ec6d543f4bd1adf58352cbb3bd67
florian <florian@videodesk.com>
2016-09-19 17:32:07 +0200

    improvment

--- scripts/bo.sh
+++ scripts/bo.sh
@@ -38,9 +38,9 @@ git pull
 git status
 cd VideoDesk/Symfony
 sh bin/deploy_dev.sh
-php app/console cache:clear --env=dev --no-debug;
-php app/console cache:clear --env=prod --no-debug;
-php app/console assetic:dump --env=prod --no-debug;
-php app/console assets:install web
+#php app/console cache:clear --env=dev --no-debug;
+#php app/console cache:clear --env=prod --no-debug;
+#php app/console assetic:dump --env=prod --no-debug;
+#php app/console assets:install web
 
 echo 'flush_all' | nc localhost 11211
